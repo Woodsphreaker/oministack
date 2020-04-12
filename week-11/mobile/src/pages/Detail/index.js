7eb909d9894c6e7863efb1983bb5fc924ba03b22
Carlo Enrico <carlo.enrico@uol.com.br>
2020-04-11 23:24:29 -0300

    chore: detail screen done

--- week-11/mobile/src/pages/Detail/index.js
+++ week-11/mobile/src/pages/Detail/index.js
@@ -1,20 +1,91 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Feather } from '@expo/vector-icons'
 
 import LogoImage from '../../assets/img/logo.png'
 
-import { Container, Logo, Header, HeaderContent, TextBold } from './styles'
+import {
+  Container,
+  Logo,
+  Header,
+  TextBold,
+  BackButton,
+  ItemBox,
+  ItemContainer,
+  Item,
+  Property,
+  Value,
+  ContactBox,
+  HeroTitle,
+  HeroDescription,
+  ActionContainer,
+  ActionButton,
+  ActionButtonText,
+} from './styles'
 
 const Detail = ({ navigation }) => {
+  const handleClick = () => {
+    navigation.goBack()
+  }
+
   return (
     <Container>
       <Header>
         <Logo source={LogoImage} />
-        <HeaderContent>
-          Total de
-          <TextBold> 0 casos</TextBold>
-        </HeaderContent>
+        <BackButton onPress={handleClick}>
+          <Feather name="arrow-left" size={28} color="#e82041" />
+        </BackButton>
       </Header>
+
+      <ItemBox>
+        <Item>
+          <ItemContainer style={{ marginTop: 0 }}>
+            <Property>
+              <TextBold>ONG:</TextBold>
+            </Property>
+            <Value>APAD</Value>
+          </ItemContainer>
+
+          <ItemContainer>
+            <Property>
+              <TextBold>CASO: </TextBold>
+            </Property>
+            <Value>Cadelinha Atropelada</Value>
+          </ItemContainer>
+
+          <ItemContainer>
+            <Property>
+              <TextBold>VALOR:</TextBold>
+            </Property>
+            <Value>R$ 120,00</Value>
+          </ItemContainer>
+        </Item>
+      </ItemBox>
+
+      <ContactBox>
+        <Item>
+          <HeroTitle>
+            <TextBold>Salve o dia!</TextBold>
+          </HeroTitle>
+          <HeroTitle>
+            <TextBold>Seja o Herói desse caso. </TextBold>
+          </HeroTitle>
+          <HeroDescription>Entre em contato</HeroDescription>
+
+          <ActionContainer>
+            <ActionButton onPress={() => {}}>
+              <ActionButtonText>
+                <TextBold>WhatsApp</TextBold>
+              </ActionButtonText>
+            </ActionButton>
+            <ActionButton onPress={() => {}}>
+              <ActionButtonText>
+                <TextBold>E-mail</TextBold>
+              </ActionButtonText>
+            </ActionButton>
+          </ActionContainer>
+        </Item>
+      </ContactBox>
     </Container>
   )
 }
--- week-11/mobile/src/pages/Detail/styles.js
+++ week-11/mobile/src/pages/Detail/styles.js
@@ -25,5 +25,81 @@ const HeaderContent = styled.Text`
 const TextBold = styled.Text`
   font-weight: bold;
 `
+const BackButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+const ItemBox = styled.View`
+  margin-top: 32px;
+`
+const ItemContainer = styled.View`
+  margin: 15px 0 0px 0;
+`
+const Item = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background: #fff;
+  margin-bottom: 16px;
+`
+const Property = styled.Text`
+  font-size: 14px;
+  color: #41414d;
+`
+const Value = styled.Text`
+  margin-top: 8px;
+  font-size: 15px;
+  color: #737380;
+`
+const ContactBox = styled(ItemBox)`
+  margin-top: 0;
+`
+const HeroTitle = styled.Text`
+  font-size: 18px;
+  color: #13131a;
+  line-height: 30px;
+`
+const HeroDescription = styled.Text`
+  font-size: 15px;
+  color: #737380;
+  margin-top: 16px;
+`
+const ActionContainer = styled.View`
+  margin-top: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+`
 
-export { Container, Header, Logo, HeaderContent, TextBold }
+const ActionButton = styled.TouchableOpacity`
+  background: #e02041;
+  border-radius: 8px;
+  height: 50px;
+  margin: 5px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+const ActionButtonText = styled.Text`
+  color: #fff;
+  font-size: 15px;
+`
+
+export {
+  Container,
+  Header,
+  Logo,
+  HeaderContent,
+  TextBold,
+  BackButton,
+  ItemBox,
+  ItemContainer,
+  Item,
+  Property,
+  Value,
+  ContactBox,
+  HeroTitle,
+  HeroDescription,
+  ActionContainer,
+  ActionButton,
+  ActionButtonText,
+}
